a9ac9911d92833e9aceebf5f1b85ccdbb24022dc
ricardofisma <ricardomallqui6@gmail.com>
2020-05-12 18:49:24 -0500

    w

--- conexion.php
+++ conexion.php
@@ -1,7 +1,7 @@
 <?php
     
     try{
-         $conexion = new PDO('mysql:host=b2fl5juvonx7jy2herss-mysql.services.clever-cloud.com;dbname=b2fl5juvonx7jy2herss', 'u2torrdwkgyxog4s', 'PPe6V7lNx0FjMHYfApZI');
+         $conexion = new PDO('host=b2fl5juvonx7jy2herss-mysql.services.clever-cloud.com;dbname=b2fl5juvonx7jy2herss', 'u2torrdwkgyxog4s', 'PPe6V7lNx0FjMHYfApZI');
     }catch(PDOException $prueba_error){
         echo "Error: " . $prueba_error->getMessage();
     }
--- login.php
+++ login.php
@@ -13,7 +13,7 @@
         $clave = hash('sha512', $clave);
         
         try{
-            $conexion = new PDO('mysql:host=localhost;dbname=login_tuto', 'root', '');
+            $conexion = new PDO('mysql:host=b2fl5juvonx7jy2herss-mysql.services.clever-cloud.com;dbname=b2fl5juvonx7jy2herss', 'u2torrdwkgyxog4s', 'PPe6V7lNx0FjMHYfApZI');
             }catch(PDOException $prueba_error){
                 echo "Error: " . $prueba_error->getMessage();
             }
