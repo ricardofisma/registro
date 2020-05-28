f0d8550656ef6513724e6f331077166092310b49
ricardofisma <ricardomallqui6@gmail.com>
2020-05-28 11:28:16 -0500

    w

--- register.php
+++ register.php
@@ -23,8 +23,8 @@
             $error .= '<i>Favor de rellenar todos los campos</i>';
         }else{
             try{
-                $conexion = new PDO('mysql:host=localhost;dbname=proyecto', 'root', '');
-         //$conexion = new PDO('mysql:host=b2fl5juvonx7jy2herss-mysql.services.clever-cloud.com;dbname=b2fl5juvonx7jy2herss', 'u2torrdwkgyxog4s', 'PPe6V7lNx0FjMHYfApZI');
+            //    $conexion = new PDO('mysql:host=localhost;dbname=proyecto', 'root', '');
+         $conexion = new PDO('mysql:host=b2fl5juvonx7jy2herss-mysql.services.clever-cloud.com;dbname=b2fl5juvonx7jy2herss', 'u2torrdwkgyxog4s', 'PPe6V7lNx0FjMHYfApZI');
             }catch(PDOException $prueba_error){
                 echo "Error: " . $prueba_error->getMessage();
             }
