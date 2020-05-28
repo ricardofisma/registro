896330c0c84958105e9ed3a1b4ddb5a31092257b
ricardofisma <ricardomallqui6@gmail.com>
2020-05-28 12:24:56 -0500

    w

--- login.php
+++ login.php
@@ -13,8 +13,8 @@
         $clave = hash('sha512', $clave);
         
         try{
-            $conexion = new PDO('mysql:host=localhost;dbname=proyecto', 'root', '');
-        //         $conexion = new PDO('mysql:host=b2fl5juvonx7jy2herss-mysql.services.clever-cloud.com;dbname=b2fl5juvonx7jy2herss', 'u2torrdwkgyxog4s', 'PPe6V7lNx0FjMHYfApZI');    
+           // $conexion = new PDO('mysql:host=localhost;dbname=proyecto', 'root', '');
+                 $conexion = new PDO('mysql:host=b2fl5juvonx7jy2herss-mysql.services.clever-cloud.com;dbname=b2fl5juvonx7jy2herss', 'u2torrdwkgyxog4s', 'PPe6V7lNx0FjMHYfApZI');    
         }catch(PDOException $prueba_error){
                 echo "Error: " . $prueba_error->getMessage();
             }
