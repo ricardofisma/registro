bf312cdd20759e4968268fa1f3f7fbb42cea7538
ricardofisma <ricardomallqui6@gmail.com>
2020-06-03 10:10:08 -0500

    w

--- conexion.php
+++ conexion.php
@@ -1,8 +1,8 @@
 <?php
     
     try{
-         $conexion = new PDO('mysql:host=localhost;dbname=proyecto', 'root', '');
-        // $conexion = new PDO('mysql:host=b2fl5juvonx7jy2herss-mysql.services.clever-cloud.com;dbname=b2fl5juvonx7jy2herss', 'u2torrdwkgyxog4s', 'PPe6V7lNx0FjMHYfApZI');
+       //  $conexion = new PDO('mysql:host=localhost;dbname=proyecto', 'root', '');
+         $conexion = new PDO('mysql:host=b2fl5juvonx7jy2herss-mysql.services.clever-cloud.com;dbname=b2fl5juvonx7jy2herss', 'u2torrdwkgyxog4s', 'PPe6V7lNx0FjMHYfApZI');
     }catch(PDOException $prueba_error){
         echo "Error: " . $prueba_error->getMessage();
     }
